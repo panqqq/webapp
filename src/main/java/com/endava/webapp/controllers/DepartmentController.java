0d8959cc6e60049264ed9441b2912baf8faa444c
Pantelimon Pietraru <pietrarupantelimon@gmail.com>
2022-10-21 16:10:55 +0300

    add updated pom & remove a blank line

--- src/main/java/com/endava/webapp/controllers/DepartmentController.java
+++ src/main/java/com/endava/webapp/controllers/DepartmentController.java
@@ -12,15 +12,15 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping("departments")
 public class DepartmentController {
+
     private final DepartmentService departmentService;
+
     @Autowired
     public DepartmentController(DepartmentService departmentService) {
         this.departmentService = departmentService;
